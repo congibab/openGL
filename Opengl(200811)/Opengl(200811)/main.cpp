ee2af0e9ccfd831bc6fdd2928098bf151d4aaccc
congibab <engljapen2@gmail.com>
2020-08-11 22:16:40 +0900

    success shader file read

--- Opengl(200811)/Opengl(200811)/main.cpp
+++ Opengl(200811)/Opengl(200811)/main.cpp
@@ -1,38 +1,155 @@
-/**
- * From the OpenGL Programming wikibook: http://en.wikibooks.org/wiki/OpenGL_Programming
- * This file is in the public domain.
- * Contributors: Sylvain Beucler
- */
+
 #include <stdio.h>
 #include <stdlib.h>
- /* Use glew.h instead of gl.h to get all the GL prototypes declared */
+#include <cstdlib>
+
 #include <GL/glew.h>
-/* Using the GLUT library for the base windowing setup */
 #include <GL/freeglut.h>
+
+#include <iostream>
+
+using namespace std;
+
+GLuint program;
+GLint attribute_coord2d;
+
+int Init_resources()
+{
+	GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
+	
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	const char* vs_source=
+	
+	#ifdef GL_ES_VERSION_2_0
+		"#version 100\n"  // OpenGL ES 2.0
+	#else
+		"#version 120\n"  // OpenGL 2.1
+	#endif
+			"attribute vec2 coord2d;                  "
+			"void main(void) {                        "
+			"  gl_Position = vec4(coord2d, 0.0, 1.0); "
+			"}";
+	glShaderSource(vs, 1, &vs_source, NULL);
+	glCompileShader(vs);
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &compile_ok);
+	if (!compile_ok)
+	{
+		cout << "Error iin vertex shadder" << endl;
+		return 0;
+	}
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	const char *fs_source =
+
+	#ifdef GL_ES_VERSION_2_0
+			"#version 100\n"  // OpenGL ES 2.0
+	#else
+			"#version 120\n"  // OpenGL 2.1
+	#endif
+			"void main(void) {        "
+			"  gl_FragColor[0] = 0.0; "
+			"  gl_FragColor[1] = 0.0; "
+			"  gl_FragColor[2] = 1.0; "
+			"}";
+	glShaderSource(fs, 1, &fs_source, NULL);
+	glCompileShader(fs);
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &compile_ok);
+	if (!compile_ok)
+	{
+		cout << "Error iin vertex shadder" << endl;
+		return 0;
+	}
+
+	program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
+	if (!link_ok)
+	{
+		cout << "glLinkProgram:";
+		return 0;
+	}
+
+	const char* attribute_name = "coord2d";
+	attribute_coord2d = glGetAttribLocation(program, attribute_name);
+	if (attribute_coord2d == -1)
+	{
+		cout << "Could not bind attribute" << attribute_name << endl;
+		return 0;
+	}
+	return 1;
+}
 
 void display(void)
 {
+	glClearColor(1.0, 1.0, 1.0, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glUseProgram(program);
+	glEnableVertexAttribArray(attribute_coord2d);
+	GLfloat triangle_vertices[] =
+	{
+		0.8,  0.8,
+	   -0.8, -0.8,
+		0.8, -0.8,
+	};
 
-	glColor3f(0.5, 0.5, 0.5);
-	glBegin(GL_TRIANGLES);
-	glVertex3f(0.0, 0.5, 0);
-	glVertex3f(-0.5, -0.5, 0);
-	glVertex3f(0.5, -0.5, 0);
-	glEnd();
-	glFinish();
+	glVertexAttribPointer(
+		attribute_coord2d,
+		2,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		triangle_vertices
+	);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(attribute_coord2d);
+	glutSwapBuffers();
+	
 }
 
+void free_resource()
+{
+	glDeleteProgram(program);
+}
 
+void Reshape(int NewWidth, int NewHeight)
+{
+	glViewport(0, 0, NewWidth, NewHeight);
+	GLfloat WidthFactor = (GLfloat)NewWidth / (GLfloat)300;
+	GLfloat HeightFactor = (GLfloat)NewHeight / (GLfloat)300;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0*WidthFactor, 1.0*WidthFactor, -1.0* HeightFactor, 1.0 * HeightFactor, -1.0, 1.0);
+
+}
 
 int main(int argc, char* argv[])
 {
-
 	glutInit(&argc, argv);
-	glutCreateWindow(argv[0]);
-	glClearColor(1.0, 1.0, 1.0, 1);
-	glutDisplayFunc(display);
-	glutMainLoop();
+	glutInitContextVersion(2, 0);
+	glutInitDisplayMode(GLUT_RGBA |GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(1200, 100);
+	glutCreateWindow("open GL");
+	
+	GLenum glew_status = glewInit();
+	if (glew_status != GLEW_OK)
+	{
+		cout << "Error:" << glewGetErrorString(glew_status) << endl;
+		return 1;
+	}
+
+	if (Init_resources())
+	{
+		glutDisplayFunc(display);
+		glutReshapeFunc(Reshape);
+		glutMainLoop();
+	}
+
+	free_resource();
 	return 0;
 
 }
