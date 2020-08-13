aedf5a7396498377d4c726e5dc5515659dc3c976
congibab <engljapen2@gmail.com>
2020-08-14 03:00:45 +0900

    draw grid

--- snakeGame/snakeGame/Main.cpp
+++ snakeGame/snakeGame/Main.cpp
@@ -1,27 +1,49 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <glm/glm.hpp>
 
 #include <iostream>
 
+using namespace std;
+
+GLfloat off_set_x = 0;
+GLfloat off_set_y = 0;
+
+GLfloat camera_x = 0;
+GLfloat camera_y = 0;
+
+
 void display()
 {
+	glLineWidth(1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3d(1.0, 0.0, 0.0);
-	glBegin(GL_LINE_LOOP);
-	glVertex2d(-0.5, 0.5);
-	glVertex2d(-0.5, -0.5);
-	glVertex2d(0.5, -0.5);
-	glVertex2d(0.5, 0.5);
-	glEnd();
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{	
+			glBegin(GL_LINE_LOOP);
+			glVertex2d(i - 0.5 - off_set_x, j + 0.5 - off_set_y);
+			glVertex2d(i - 0.5 - off_set_x, j - 0.5 - off_set_y);
+			glVertex2d(i + 0.5 - off_set_x, j - 0.5 - off_set_y);
+			glVertex2d(i + 0.5 - off_set_x, j + 0.5 - off_set_y);
+			glEnd();
+		}
+	}
 	glFinish();
 }
 
 void reshape(GLint w, GLint h)
 {
-	glViewport(0, 0, w, h);
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-w / 200.0, w / 200.0, -h / 200.0, h / 200.0, -1.0, 1.0);
+	//glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+	glOrtho(-w / 50.0, w / 50.0, -h / 50.0, h / 50.0, -1.0, 1.0);
+	
+	cout << "width:" << w << " height:" << h << endl;
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -30,9 +52,36 @@ void keyboard(unsigned char key, int x, int y)
 	{
 	case 'q':
 	case 'Q':
-		std::cout << "test" << std::endl;
+		cout << "Input key q" << endl;
 		break;
-	case '\033':
+	//================================
+	case 'a':
+	case 'A':
+		off_set_x += 0.5;
+		glutPostRedisplay();
+		cout << "Input key a" << endl;
+		break;
+	case 'd':
+	case 'D':
+		off_set_x -= 0.5;
+		glutPostRedisplay();
+		cout << "Input key d" << endl;
+		break;
+	case 'w':
+	case 'W':
+		off_set_y -= 0.5;
+		glutPostRedisplay();
+		cout << "Input key w" << endl;
+		break;
+	case 's':
+	case 'S':
+		off_set_y += 0.5;
+		glutPostRedisplay();
+		cout << "Input key s" << endl;
+		break;
+	//--------------------------------
+	case 27:
+		cout << "Input key Esc" << endl;
 		exit(0);
 		break;
 	default:
